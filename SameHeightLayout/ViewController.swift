f74a01275d2ca91529682062ba017c8beed0b583
nathanwhy <nathanw@126.com>
2016-01-10 22:22:40 +0800

    Initial Commit

---
+++ SameHeightLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SameHeightLayout
+//
+//  Created by nathan on 16/1/10.
+//  Copyright © 2016年 nathanwhy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
